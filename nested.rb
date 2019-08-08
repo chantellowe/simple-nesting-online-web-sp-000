98c99d991cf59590d7915215341f47534ac3af55
Chantel Lowe <chantellowe17@gmail.com>
2019-08-08 14:01:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 
-  programmer_hash.unshift([:yukihiro_matsumoto])
+  programmer_hash[:yukihiro_matsumoto]
     
 end
 
@@ -105,6 +105,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[alan_kay][known_for] = "GUI"
      
 end
 
