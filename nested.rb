27b79aca25ef704f637e05e677ecfc1eb24ed570
Chantel Lowe <chantellowe17@gmail.com>
2019-08-08 13:57:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,10 +82,7 @@ def adding_matz
         }
      }
 
-  programmer_hash.unshift([:yukihiro_matsumoto => {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-  }])
+  programmer_hash.unshift([:yukihiro_matsumoto])
     
 end
 
